6d658b4b8f673aea0237b7c570f9cb9a4e0cfed7
nabilsiddik <nabilsiddik90@gmail.com>
2024-07-08 09:41:42 -0700

    Type added

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello World")
+#printing hello world
+print("Hello World", "How are you?")
+
+#type
+name = "Nabil Siddik"
+age = 25
+price = 25.99
+
+print(type(name))
+print(type(age))
+print(type(price))
